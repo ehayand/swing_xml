b9edb9dbc41e5006ea2f3aee363461253da4601f
Jaeyu Jo <ehay@naver.com>
2018-11-21 09:15:15 +0900

    Init

---
+++ xml/src/XmlApplication.java
@@ -0,0 +1,12 @@
+/**
+ * Created by ehay@naver.com on 2018-11-21
+ * Blog : http://ehay.tistory.com
+ * Github : http://github.com/ehayand
+ */
+
+public class XmlApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
