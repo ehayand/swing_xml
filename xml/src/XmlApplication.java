e81b13585dd637b5c7c9d973b4d0b43393c572ac
Jaeyu Jo <ehay@naver.com>
2018-12-01 23:20:33 +0900

    Update XmlApplication.java

--- xml/src/XmlApplication.java
+++ xml/src/XmlApplication.java
@@ -1,6 +1,5 @@
 import io.Input;
 import model.Node;
-import model.Xml;
 import process.NodeProcess;
 import process.XmlCheckProcess;
 
@@ -12,7 +11,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Vector;
 
 /**
@@ -23,102 +21,101 @@ import java.util.Vector;
 
 public class XmlApplication extends JFrame implements TreeSelectionListener, ActionListener {
 
-        JTree tree;
-        JLabel label;
-        JPanel panel;
-//        JButton check;
-        JTextArea textArea;
+    JTree tree;
+    JLabel label;
+    JPanel panel;
+    //        JButton check;
+    JTextArea textArea;
 
-        public XmlApplication() throws IOException {
-            //프레임 설정
-            setSize(500, 500);
-            setTitle("XML 에디터");
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public XmlApplication() throws IOException {
+        //프레임 설정
+        setSize(500, 500);
+        setTitle("XML 에디터");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            Container container = getContentPane();
-            container.setLayout(new BorderLayout());
-
-
-            //파일 읽기, 트리구조화
-            Input in = new Input();
-            Node root = in.process();
-
-            NodeProcess nodeProcess = new NodeProcess(root, in.getXmls());
-            XmlCheckProcess xmlCheckProcess = new XmlCheckProcess();
-            xmlCheckProcess.process(root);
+        Container container = getContentPane();
+        container.setLayout(new BorderLayout());
 
 
+        //파일 읽기, 트리구조화
+        Input in = new Input();
+        Node root = in.process();
 
-            //그래픽
-            panel = new JPanel();
-            label = new JLabel("TEXT");
+        NodeProcess nodeProcess = new NodeProcess(root, in.getXmls());
+        nodeProcess.process();
+        XmlCheckProcess xmlCheckProcess = new XmlCheckProcess();
+        xmlCheckProcess.process(root);
 
-            if(!xmlCheckProcess.isError()) {
-                tree = new JTree(root);
 
-                DefaultTreeCellRenderer renderer = new DefaultTreeCellRenderer();
-                renderer.setLeafIcon(null);
-                renderer.setOpenIcon(null);
-                renderer.setClosedIcon(null);
+        //그래픽
+        panel = new JPanel();
+        label = new JLabel("TEXT");
 
-                tree.setCellRenderer(renderer);
-                tree.addTreeSelectionListener(this);
+        if (!xmlCheckProcess.isError()) {
+            tree = new JTree(root);
 
-                container.add(new JScrollPane(tree), BorderLayout.CENTER);
-            }
-            else {
-                container.add(new JTextField("ERROR"), BorderLayout.CENTER);
-            }
+            DefaultTreeCellRenderer renderer = new DefaultTreeCellRenderer();
+            renderer.setLeafIcon(null);
+            renderer.setOpenIcon(null);
+            renderer.setClosedIcon(null);
 
-            textArea = new JTextArea(xmlCheckProcess.getErrorMessage(), 5, 44);
-            textArea.setEditable(false);
-            panel.add(new JScrollPane(textArea), BorderLayout.SOUTH);
+            tree.setCellRenderer(renderer);
+            tree.addTreeSelectionListener(this);
+
+            container.add(new JScrollPane(tree), BorderLayout.CENTER);
+        } else {
+            container.add(new JTextField("ERROR"), BorderLayout.CENTER);
+        }
+
+        textArea = new JTextArea(xmlCheckProcess.getErrorMessage(), 5, 44);
+        textArea.setEditable(false);
+        panel.add(new JScrollPane(textArea), BorderLayout.SOUTH);
 
 //            check = new JButton("검사");
 //            check.addActionListener(this);
 //            panel.add(check, BorderLayout.EAST);
 
-            //컨테이너 설정
-            container.add(panel, BorderLayout.NORTH);
-            container.add(label, BorderLayout.SOUTH);
+        //컨테이너 설정
+        container.add(panel, BorderLayout.NORTH);
+        container.add(label, BorderLayout.SOUTH);
 
-            //프레임 설정
-            setVisible(true);
-        }
+        //프레임 설정
+        setVisible(true);
+    }
 
-        @Override
-        public void actionPerformed(ActionEvent e) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
 
-        }
+    }
 
-        @Override
-        public void valueChanged(TreeSelectionEvent e) {
-            Node node = (Node) tree.getLastSelectedPathComponent();
-            if(node == null) return;
-            StringBuilder newText = new StringBuilder();
+    @Override
+    public void valueChanged(TreeSelectionEvent e) {
+        Node node = (Node) tree.getLastSelectedPathComponent();
+        if (node == null) return;
+        StringBuilder newText = new StringBuilder();
 
-            newText.append("선택노드 : ");
-            newText.append(node.getIdx() + " ");
-            newText.append((String) node.getUserObject());
-            Vector<Node> children = node.getChildren();
+        newText.append("선택노드 : ");
+        newText.append(node.getIdx() + " ");
+        newText.append((String) node.getUserObject());
+        Vector<Node> children = node.getChildren();
 
-            newText.append(" , 자식노드 : ");
-            if(children != null) {
-                for (Node child : children) {
-                    newText.append(child.getIdx() + " ");
-                    newText.append((String) child.getUserObject()).append(", ");
-                }
-
-                newText.delete(newText.length()-2, newText.length());
-            } else {
-                newText.append("없음");
+        newText.append(" , 자식노드 : ");
+        if (children != null) {
+            for (Node child : children) {
+                newText.append(child.getIdx() + " ");
+                newText.append((String) child.getUserObject()).append(", ");
             }
 
-            label.setText(newText.toString());
+            newText.delete(newText.length() - 2, newText.length());
+        } else {
+            newText.append("없음");
         }
 
-        public static void main(String[] args) throws IOException {
-            new XmlApplication();
-        }
+        label.setText(newText.toString());
     }
+
+    public static void main(String[] args) throws IOException {
+        new XmlApplication();
+    }
+}
